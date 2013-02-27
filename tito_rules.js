0ebe8c411490e0cbb3d5caa88f358359c22a08c2
Yulia <y.bobkova@ps-webforge.com>
2013-02-27 16:18:30 +0100

    rules sind lesbarer, kommentare sind highlightet

--- tito_rules.js
+++ tito_rules.js
@@ -5,6 +5,16 @@ var oop = require("pilot/oop");
 var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
 
 var MyNewHighlightRules = function() {
+  
+    var identifier = "[a-zA-ZöäüÖÄÜ]";
+    var soundoperator = "\\s*\\>\\s*|\\s*\\*\\s*\\>";
+    var gotooperator = "^\\s*\\=\\s*\\>";
+    var ifelse = "Oder|Wenn|Ansonsten";
+    var soundtype = "\\[|\\]|\\(|\\)";
+    var preposition = "(?:aus|zu|zum|zur|in|jedes|für)";
+    var declaration = "\\s*\\:\\=\\s*";
+    var operator = "\\<|\\>|\\=\\=|\\>\\=|\\<\\=";
+
 
     // regexp must not have capturing parentheses. Use (?:) instead.
     // regexps are ordered -> the first match is used
@@ -16,26 +26,31 @@ var MyNewHighlightRules = function() {
                 regex: "Spiel:\\s*"
             },
             {
-                token: "support.function", // Variablendeklaration, keine Tabelle
-                regex: "[a-zA-ZöäüÖÄÜ]+(?=\\s*\\:\\=\\s*\\d+)"
+                token: "variable", // Variablendeklaration, keine Tabelle
+                regex: identifier+"+(?="+declaration+"\\d+)"
             },
             {
-                token: "support.function", // Wenn nach := eine variable steht
-                regex: "[a-zA-ZöäüÖÄÜ]+(?!\\s*\\:\\=\\s*\\d+)(?=\\s*\\:\\=\\s*)",
+                token: "variable", // Wenn nach := eine variable steht
+                regex: identifier+"+(?!"+declaration+"\\d+)(?="+declaration+")",
                 next: "variable"
             },
             {
-                token: "text",
-                regex: "\\s*\\:",
-                next: "tableUndZeile"
+                token: "comment", // Wenn nach := eine variable steht
+                regex: "\\#",
+                next: "comment"
             },
             {
-                token: "string", // Anfang einer Funktiondeklaration
-                regex: "^[a-zA-ZöäüÖÄÜ\\s]+\\:\\s*$"
+                token: "planetext",
+                regex: "\\s*\\:",
+                next: "declaration"
+            },
+            {
+                token: "functions", // Anfang einer Funktiondeklaration
+                regex: "^.*\\:\\s*$"
             },
             {
                 token: "keyword", //Kontrollesymbole
-                regex: "Oder|Wenn|Ansonsten"
+                regex: ifelse
             },
             {
                 token: "keyword", //"Für jedes"
@@ -44,150 +59,188 @@ var MyNewHighlightRules = function() {
             },
             {
                 token: "keyword", //Goto
-                regex: "^\\s*\\=\\s*\\>",
+                regex: gotooperator,
                 next: "gotofunction"
             },
             {
-                token: "comment", // Soundsymbole
-                regex: "\\s*\\>\\s*|\\s*\\*\\s*\\>",
+                token: "sound", // Soundsymbole
+                regex: soundoperator,
                 next: "sound"
             },
             {
-                token: "support.function", // Variablen mit < oder > oder ==
-                regex: "[a-zA-ZöäüÖÄÜ]+\\s*(?=\\<|\\>|\\=\\=|\\>\\=|\\<\\=\\s*)",
+                token: "variable", // Variablen mit < oder > oder ==
+                regex: identifier+"+\\s*(?="+operator+"\\s*)",
                 next: "variable"
             },
             {
-                token: "support.function", // Variablen mit < oder > oder == einer Value
-                regex: "[a-zA-ZöäüÖÄÜ]+(?=\\s*(?:\\<|\\>|\\=\\=|\\>\\=|\\<\\=)\\s*\\d+)"
+                token: "variable", // Variablen mit < oder > oder == einer Value
+                regex: identifier+"+(?=\\s*(?:"+operator+")\\s*\\d+)"
             },
             {
-                token: "text",
+                token: "planetext",
                 regex: "^\\s*Füge\\s*",
                 next: "fuegeeinen"
             },
             {
-                token: "text",
+                token: "planetext",
                 regex: "(?:Wähle|auf)\\s*",
                 next: "waehle"
             },
             {
-                token: "text",
-                regex: "(?:aus|zu|zum|zur|in|jedes|für)\\s+",
+                token: "planetext",
+                regex: preposition+"\\s+",
                 next: "variableImText"
             }
           ],
           variable: [
             {
-                token: "support.function",
-                regex: "\\s*[a-zA-ZöäüÖÄÜ]"
+                token: "variable",
+                regex: "\\s*"+identifier
             },
             {
-                token: "text",
+                token: "planetext",
                 regex: "true|false|\\s|$",
                 next: "start"
             }
           ],
+          comment: [
+            {
+                token: "planetext",
+                regex: "$",
+                next: "start"
+            },
+            {
+                token: "comment",
+                regex: ".*"
+            }
+          ],
           waehle: [
             {
-                token: "text",
+                token: "planetext",
                 regex: "(?:einen|eine|ein|von der)\\s+",
                 next: "variableImText"
             },
             {
-                token: "text",
+                token: "planetext",
                 regex: "etwas",
                 next: "start"
             },
             {
-                token: "text",
+                token: "planetext",
                 regex: "[0-9]+\\s*",
                 next: "variableNachZahl"
             },
             {
-                token: "support.function",
-                regex: "[a-zA-ZöäüÖÄÜ]+",
+                token: "variable",
+                regex: identifier+"+",
                 next: "start"
             }
           ],
           fuegeeinen: [
             {
-                token: "text",
+                token: "planetext",
                 regex: "(?:einen|eine|ein|von der)\\s+",
                 next: "variableImText"
             },
             {
-                token: "support.function",
-                regex: "[a-zA-ZöäüÖÄÜ]+",
+                token: "variable",
+                regex: identifier+"+",
                 next: "start"
             }
           ],
           variableImText:[
             {
-                token: "support.function",
-                regex: "[a-zA-ZöäüÖÄÜ]"
+                token: "variable",
+                regex: identifier
             },
             {
-                token: "text",
-                regex: "\\s*[0-9]+\\s*",
+                token: "planetext",
+                regex: "\\s*\\d+\\s*",
                 next: "variableNachZahl"
             },
             {
-                token: "text",
+                token: "planetext",
                 regex: "\\s",
                 next: "start"
             }
           ],
           variableNachZahl:[
             {
-                token: "text",
+                token: "planetext",
                 regex: "\\s|$",
                 next: "start"
             },
             {
-                token: "support.function",
-                regex: "[a-zA-ZöäüÖÄÜ]+"
+                token: "variable",
+                regex: identifier+"+"
             },
           ],
           sound: [
             {
-                token: "text",
+                token: "planetext",
                 regex: "$",
                 next: "start"
             },
             {
-                token: "comment",
-                regex: "\\[|\\]|\\(|\\)"
+                token: "sound",
+                regex: soundtype
             },
             {
-                token: "text",
+                token: "planetext",
                 regex: "\\s*\\:\\s*",
                 next: "variable"
             }
           ],
           gotofunction: [
             {
-                token: "text",
+                token: "planetext",
                 regex: "$",
                 next: "start"
             },
             {
-                token: "constant.numeric",
+                token: "gotofunction",
                 regex: ".*"
             }
           ],
-          tableUndZeile: [
+          declaration: [
             {
-                token: "text",
+                token: "planetext",
                 regex: "true|false|$|table",
                 next: "start"
             },
             {
-                token: "support.function",
-                regex: "[a-zA-ZöäüÖÄÜ]"
+                token: "variable",
+                regex: identifier
             }
           ]
     };
+        var tokenMap = {
+        "variable": "support.function",
+        "functions": "string",
+        "sound": "comment",
+        "gotofunction": "constant.language.boolean",
+        "planetext": "text",
+        "comment": "constant.numeric"
+    };
+    
+    for (var state in this.$rules) {
+        var stateRules = this.$rules[state];
+        for (var i = stateRules.length; i--; ) {
+            var rule = stateRules[i];
+            if (rule.include || typeof rule == "string") {
+                var args = [i, 1].concat(this.$rules[rule.include || rule]);
+                if (rule.noEscape) {
+                    args = args.filter(function(x) {
+                        return !x.next;
+                    });
+                }
+                stateRules.splice.apply(stateRules, args);
+            } else if (rule.token in tokenMap) {
+                rule.token = tokenMap[rule.token];
+            }
+        }
+    }
+
 };
 
 oop.inherits(MyNewHighlightRules, TextHighlightRules);
